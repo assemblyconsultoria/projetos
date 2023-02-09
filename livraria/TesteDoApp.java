a0111a498ddbcee7fb7652c82c18ae99ed95e4f0
Assembly Consultoria <assemblyconsultoria@gmail.com>
2023-02-09 00:05:47 -0300

    Testando uma classe Java

---
+++ livraria/TesteDoApp.java
@@ -0,0 +1,8 @@
+package livraria;
+
+public class TesteDoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Testando essa bagaça, funciona ou não funciona!!!");
+    }
+}
